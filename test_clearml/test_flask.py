5aec4180768f814ce62fba1f15a7ddde1169dc6d
pengbinlong <pengbinlong@tezign.com>
2021-01-18 14:49:04 +0800

    测试工程代码

---
+++ test_clearml/test_flask.py
@@ -0,0 +1,31 @@
+import requests
+from PIL import Image
+from io import BytesIO
+from flask import Flask, request
+from clearml import Task
+import matplotlib.pyplot as plt
+
+
+app = Flask(__name__)
+
+task = Task.init(project_name='pbl_example', task_name='flask', output_uri='http://180.76.145.171:30081/')
+@app.route('/test', methods=['POST'])
+def test():
+    urls = request.json['urls']
+    index = 121
+    for url in urls:
+        resp = requests.get(url, verify=False)
+        plt.subplot(index)
+        index += 1
+        # pillow读取
+        pil = Image.open(BytesIO(resp.content))
+        plt.imshow(pil)
+        # cv2读取
+        # src = cv2.imdecode(np.frombuffer(resp.content, np.uint8), cv2.IMREAD_UNCHANGED)
+        # plt.imshow(src[:, :, ::-1])
+    plt.show()
+    return {'status': 'success'}
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9080)
